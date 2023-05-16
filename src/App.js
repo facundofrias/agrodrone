cd0bb4ea6b0372821d9294be3877e4cd4bf0a03c
Frías <ffrias@ater.gob.ar>
2023-05-16 12:45:05 -0300

    Se comenzó a trabajar en los estilos de la navbar.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import CustomSelect from './components/customSelect/CustomSelect';
 function App() {
   return (
     <div className="App">
+      <div class="navbar">
+        <div class="red"></div>
+        <div class="green"></div>
+        <div class="blue"></div>
+      </div>
       <button className='btn-primary'>Choose a drone</button>
       <div className='separator'></div>
       <button className='btn-secondary' >Buy now</button>
