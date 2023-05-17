0e7927b79e47d5d167c5ecc0d1361f3b4fd58126
Frías <ffrias@ater.gob.ar>
2023-05-17 19:38:54 -0300

    Se agregaron estilos a la navbar.

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import CustomSelect from './components/customSelect/CustomSelect';
 function App() {
   return (
     <div className="App">
-      <div class="navbar">
-        <div class="red"></div>
-        <div class="green"></div>
-        <div class="blue"></div>
+      <div className="navbar">
+        <div className="main-color"></div>
+        <div className="light-grayish-blue"></div>
+        <div className="secondary-color"></div>
       </div>
       <button className='btn-primary'>Choose a drone</button>
       <div className='separator'></div>
